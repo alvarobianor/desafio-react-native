7c950839a7d12f148b350859fa2bbc7a42661b72
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-04-16 17:00:52 -0300

    dark mode on

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 
 import {
   SafeAreaView,
@@ -10,13 +10,14 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-
 import api from './services/api'
 
-
+//const [theme, setTheme] = useState({ dark_mode })
 export default function App() {
 
   const [repo, setRepos] = useState([])
+  const [theme, setTheme] = useState({ style: false })
+
 
   useEffect(() => {
     api.get('/repositories').then(e => {
@@ -25,36 +26,65 @@ export default function App() {
     })
   }, [])
 
+  function toogleTheme() {
+    if (theme.style === false) {
+      setTheme({ style: true })
+
+    } else {
+
+      setTheme({ style: false })
+    }
+  }
+
 
   async function handleLikeRepository(id) {
-    api.post(`/repositories/${id}/like`).then(res =>{
-     const newRepo = repo
-     const index = newRepo.findIndex(e => e.id === id)
-     newRepo[index].likes = res.data.likes
+    api.post(`/repositories/${id}/like`).then(res => {
+      const newRepo = repo
+      const index = newRepo.findIndex(e => e.id === id)
+      newRepo[index].likes = res.data.likes
       // console.log(like)
-     setRepos([...newRepo])
+      setRepos([...newRepo])
     })
-    
+
   }
 
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <SafeAreaView style={styles.container}>
+
+      <StatusBar barStyle="light-content"
+        backgroundColor={theme.style ? "#000000" : '#7159c1'} />
+      <SafeAreaView style={styles.container}
+      backgroundColor={theme.style ? "#000000" : '#7159c1'}>
+
+        <TouchableOpacity onPress={toogleTheme} style={styles.button}>
+          <Text
+            style={[styles.buttonTextToogle,
+            { color: theme.style ? "#FFF" : '#000000' },
+            { backgroundColor: theme.style ? '#A69F9F' : "#FFF" }]}
+          > Dark Theme {theme.style ? `on` : `off`}</Text>
+        </TouchableOpacity>
+
         <FlatList
           data={repo}
           keyExtractor={(e) => e.id}
           renderItem={({ item: e }) => (
             <>
-              <View style={styles.repositoryContainer}>
-                <Text style={styles.repository}>{e.title}</Text>
+              <View style={styles.repositoryContainer}
+                backgroundColor={theme.style ? "#A69F9F" : '#FFFF'}
+                >
+                <Text style={[styles.repository,
+                //{ color: theme.style ? "#FFF" : '#000000' },
+                { backgroundColor: theme.style ? "#A69F9F" : "#FFF" }]}
+                >{e.title}</Text>
 
-                <View style={styles.techsContainer}>
+                <View style={styles.techsContainer}
+                  backgroundColor={theme.style ? "#000000" : '#237511'}>
 
                   {
-                    e.techs.map(tech => <Text 
-                    key={`${e.id} ${Math.random()}`} 
-                    style={styles.tech}>
+                    e.techs.map(tech => <Text
+                      key={`${e.id} ${Math.random()}`}
+                      style={[styles.tech,
+                      { color: theme.style ? "#FFF" : '#000000' },]}>
                       {tech}
                     </Text>)
                   }
@@ -69,7 +99,9 @@ export default function App() {
 
                 <View style={styles.likesContainer}>
                   <Text
-                    style={styles.likeText}
+                    style={[styles.like,
+                      { color: theme.style ? "#000000" : '#000000' }]}
+                     // { backgroundColor: theme.style ? '#000000' : "#FFF" }]}
                     // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
                     testID={`repository-likes-${e.id}`}>
                     {`${e.likes} curtidas`}
@@ -78,17 +110,23 @@ export default function App() {
 
                 <TouchableOpacity
                   style={styles.button}
+
                   onPress={() => handleLikeRepository(e.id)}
                   // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
                   testID={`like-button-${e.id}`}
                 >
-                  <Text style={styles.buttonText}>Curtir</Text>
+                  <Text style={[styles.buttonTextToogle,
+                  { color: theme.style ? "#FFF" : '#000000' },
+                  { backgroundColor: theme.style ? '#000000' : "#7159c1" }]}
+                  >Curtir</Text>
                 </TouchableOpacity>
-              </View>
 
+              </View>
             </>
           )} />
+
       </SafeAreaView>
+
     </>
   );
 }
@@ -96,7 +134,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#7159c1",
+    //backgroundColor: "#7159c1",
   },
   flatList: {
     flex: 1
@@ -104,9 +142,10 @@ const styles = StyleSheet.create({
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
     padding: 20,
   },
+
   repository: {
     fontSize: 32,
     fontWeight: "bold",
@@ -119,10 +158,10 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "bold",
     marginRight: 10,
-    backgroundColor: "#04d361",
+    //backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    color: "#fff",
+    //color: "#fff",
   },
   likesContainer: {
     marginTop: 15,
@@ -141,8 +180,17 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
+    //color: "#fff",
+    //backgroundColor: "#7159c1",
     padding: 15,
+  },
+  buttonTextToogle: {
+    fontSize: 14,
+    fontWeight: "bold",
+    margin: 20,
+    //color: "#000000",
+    //backgroundColor: "#30D8CE",
+    padding: 15,
+    textAlign: "center"
   },
 });
---
+++ src/themes/theme.js
@@ -0,0 +1,7 @@
+export const dark_mode = {
+  BACKGROUND_COLOR: "#000000"
+}
+
+export const light_mode = {
+  BACKGROUND_COLOR: "#B27264"
+}
