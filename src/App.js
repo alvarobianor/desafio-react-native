e007f6b7d8f10345b46f2a884553c5d877b7dbb8
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-04-16 17:05:48 -0300

    strutured

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import {
 
 import api from './services/api'
 
-//const [theme, setTheme] = useState({ dark_mode })
 export default function App() {
 
   const [repo, setRepos] = useState([])
@@ -22,7 +21,6 @@ export default function App() {
   useEffect(() => {
     api.get('/repositories').then(e => {
       setRepos(e.data)
-      // console.log('ola', project)
     })
   }, [])
 
@@ -42,7 +40,6 @@ export default function App() {
       const newRepo = repo
       const index = newRepo.findIndex(e => e.id === id)
       newRepo[index].likes = res.data.likes
-      // console.log(like)
       setRepos([...newRepo])
     })
 
@@ -73,7 +70,6 @@ export default function App() {
                 backgroundColor={theme.style ? "#A69F9F" : '#FFFF'}
                 >
                 <Text style={[styles.repository,
-                //{ color: theme.style ? "#FFF" : '#000000' },
                 { backgroundColor: theme.style ? "#A69F9F" : "#FFF" }]}
                 >{e.title}</Text>
 
@@ -89,21 +85,14 @@ export default function App() {
                     </Text>)
                   }
 
-                  {/* <Text style={styles.tech}>
-                    ReactJS
-                  </Text>
-                  <Text style={styles.tech}>
-                    Node.js
-                  </Text> */}
+                  
                 </View>
 
                 <View style={styles.likesContainer}>
                   <Text
                     style={[styles.like,
                       { color: theme.style ? "#000000" : '#000000' }]}
-                     // { backgroundColor: theme.style ? '#000000' : "#FFF" }]}
-                    // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-                    testID={`repository-likes-${e.id}`}>
+                      testID={`repository-likes-${e.id}`}>
                     {`${e.likes} curtidas`}
                   </Text>
                 </View>
@@ -112,7 +101,6 @@ export default function App() {
                   style={styles.button}
 
                   onPress={() => handleLikeRepository(e.id)}
-                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
                   testID={`like-button-${e.id}`}
                 >
                   <Text style={[styles.buttonTextToogle,
@@ -134,7 +122,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //backgroundColor: "#7159c1",
   },
   flatList: {
     flex: 1
@@ -142,7 +129,6 @@ const styles = StyleSheet.create({
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
-    // backgroundColor: "#fff",
     padding: 20,
   },
 
@@ -158,10 +144,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "bold",
     marginRight: 10,
-    //backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    //color: "#fff",
   },
   likesContainer: {
     marginTop: 15,
@@ -180,16 +164,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
-    //color: "#fff",
-    //backgroundColor: "#7159c1",
     padding: 15,
   },
   buttonTextToogle: {
     fontSize: 14,
     fontWeight: "bold",
     margin: 20,
-    //color: "#000000",
-    //backgroundColor: "#30D8CE",
     padding: 15,
     textAlign: "center"
   },
